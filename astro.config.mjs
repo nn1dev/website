3897e30e916f9774ab224d54006152a1bfea7b41
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-01-22 09:52:30 +0000

    configure source maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,7 +40,11 @@ export default defineConfig({
     {},
   ),
   integrations: [
-    sentry(),
+    sentry({
+      org: "nn1-dev",
+      project: "website",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
     sitemap({
       filter: (page) => !EXCLUDED_ROUTES.includes(page),
     }),
